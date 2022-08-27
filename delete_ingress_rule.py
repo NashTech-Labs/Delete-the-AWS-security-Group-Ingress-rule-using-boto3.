6cd0d345790688cc62458f702b01cbc5e7c6aecb
MuZakkir Saifi <mohd.saifi@knoldus.com>
2022-08-27 16:26:01 +0530

    Update in code

--- delete_ingress_rule.py
+++ delete_ingress_rule.py
@@ -5,40 +5,35 @@ import boto3
 from botocore.exceptions import ClientError
 import json
 
-AWS_REGION = input("Please enter the AWS_REGION")
+REGION = input("Please enter the REGION")
 
-# this is the configration for the logger
+# this is the configration for the logger_for
 
-logger = logging.getLogger()
+logger_for = logging.getlogger_for()
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s: %(levelname)s: %(message)s')
 
-vpc_client = boto3.client("ec2", region_name=AWS_REGION)
+client = boto3.client("ec2", region_name=REGION)
 
 
-def del_ingress_rule(security_group_id, security_group_rule_ids):
+def del_rule(security_grp_id, rule_ids):
     try:
-        response = vpc_client.revoke_security_group_ingress(
-            GroupId=security_group_id,
-            SecurityGroupRuleIds=security_group_rule_ids)
+        res = client.revoke_security_group_ingress(
+            GroupId=security_grp_id,
+            SecurityGroupRuleIds=rule_ids)
 
     except ClientError:
-        logger.exception('It Could not delete ingress security group rule.')
+        logger_for.exception('Oops sorry, Your ingress security group rule can not be deleted.')
         raise
     else:
-        return response
+        return res
 
 
 if __name__ == '__main__':
-    # Constants
-    # SECURITY_GROUP_ID = 'sg-022ed25b68ad24c18'
-    SECURITY_GROUP_ID = input("Please enter the security group id")
-
-    SECURITY_GROUP_RULE_ID = input("Please enter the security group rule id")
-
-    # SECURITY_GROUP_RULE_ID = ['sgr-09865bbbe3982c174']
-    logger.info(f'Please wait, we are removing a security group ingress rule(s)...')
-    rule = del_ingress_rule(SECURITY_GROUP_ID, SECURITY_GROUP_RULE_ID)
-    logger.info(
-        f'Wow, Your security group ingress rule has been deleted: \n{json.dumps(rule, indent=4)}'
+    GROUP_ID = input("Please enter the security group id")
+    RULE_ID = input("Please enter the security group rule id")
+    logger_for.info(f'Please wait, we are removing a security group ingress rule(s)...')
+    result = del_rule(GROUP_ID, RULE_ID)
+    logger_for.info(
+        f'Wow, Your security group ingress rule has been deleted: \n{json.dumps(result, indent=4)}'
     )
